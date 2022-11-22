4557fc4ba3dc74e35fb319a2685b1c9e9435e5f7
tnarek <narektumanyan99@gmail.com>
2022-11-22 21:45:38 +0200

    add webpage

---
+++ sm/assets/teaser-data.js
@@ -0,0 +1,22 @@
+file_paths = {
+    "white-horse-pink": "./sm/assets/our_results/white_horse/pink_horse.jpg",
+    "white-horse-bronze": "./sm/assets/our_results/white_horse/bronze_horse.jpg",
+    "white-horse-robot": "./sm/assets/our_results/white_horse/robot_horse.jpg",
+
+    "dancing-robots": "./sm/assets/our_results/dancing_couple/robots_dancing.jpg",
+    "dancing-cartoon": "./sm/assets/our_results/dancing_couple/cartoon_dancing.jpg",
+    "dancing-wooden": "./sm/assets/our_results/dancing_couple/wooden_sculpture_dancing.jpg",
+
+
+    "cat-bunny-bears": "./sm/assets/our_results/cat_bunny_painting/bears.jpg",
+    "cat-bunny-polygon": "./sm/assets/our_results/cat_bunny_painting/polygon.jpg",
+
+
+    "fish-drawing-sharks": "./sm/assets/our_results/fish_drawing/sharks.jpg",
+    "fish-drawing-polygon": "./sm/assets/our_results/fish_drawing/polygon.jpg",
+
+
+    "stones-wedding-cake": "./sm/assets/our_results/stones/wedding_cake.jpg",
+    "stones-skyscraper": "./sm/assets/our_results/stones/skyscraper.jpg",
+    "stones-tower": "./sm/assets/our_results/stones/tower_in_snow.jpg",
+}
